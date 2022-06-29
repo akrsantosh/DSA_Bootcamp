f6f7d3120588225b045c4f009546f1d86552b48f
A.k.R.Santosh <47202184+akrsantosh@users.noreply.github.com>
2022-06-29 22:51:27 +0530

    Add files via upload

--- src/com/DSA/java/arrays/MultiDArrayList.java
+++ src/com/DSA/java/arrays/MultiDArrayList.java
@@ -1,10 +1,33 @@
 package com.DSA.java.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MultiDArrayList {
 
 	public static void main(String[] args) {
 		
+		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		Scanner in = new Scanner(System.in);
+		
+		//Initilasition of an ArrayList
+		for( int i=0; i<3;i++) {
+			list.add(new ArrayList<>());
+			
+		}
+		
+		
+		//Adding Elements
+		for( int i=0; i<3;i++) {
+			
+			for(int j=0;j<3;j++) {
+			list.get(i).add(in.nextInt());
+			}
+			
+		}
+			System.out.println(list);
 		
 	}
-
 }
+
